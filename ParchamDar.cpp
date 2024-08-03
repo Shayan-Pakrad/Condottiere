222133c982ccfb4224abfcf4a1e383e71b9675ac
Arvin Mahlooji <arvinmahlooji1383@gmail.com>
2024-08-03 18:24:12 +0330

    fixing the bug of parchamdar.cpp syntax

--- ParchamDar.cpp
+++ ParchamDar.cpp
@@ -1,4 +1,4 @@
 #include "ParchamDar.h"
 
-ParchamDar::ParchamDar() : SpecialCard(0, "matarsak") {}
+ParchamDar::ParchamDar() : SpecialCard(0, "parchamdar") {}
 void ParchamDar::applyEffect(Player &player) {};
